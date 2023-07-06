26cd8605acfcb48855a7601807ffdfb293a7b1e2
Kaleb Burris <kaleb.r.burris@gmail.com>
2023-07-05 20:03:31 -0800

    S1S-40 msgpack limit fix

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,79 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+// Local libraries
 mod vec;
-use vec::{Vec3, Color};
-use std::io::{stderr, Write};
+use std::f64::INFINITY;
+
+use rand::Rng;
+use vec::{Color, Point3, Vec3};
+mod ray;
+use ray::Ray;
+mod objects;
+use objects::*;
+mod camera;
+use camera::*;
+
+// Extrernal libraries
+use indicatif::ProgressIterator;
+
+fn ray_color(ray: &Ray, world: &HittableList, depth: u64) -> Color {
+    if depth == 0 { return Color::new(0.0, 0.0, 0.0) }
+
+    if let Some(record) =  world.hit(ray, 0.001, INFINITY) {
+        let target = record.p + record.normal + Vec3::random_in_unit_sphere().normalized();
+        let ray = Ray::new(record.p, target - record.p);
+        0.5 * ray_color(&ray, world, depth - 1)
+    }
+    else {
+        let unit_direction: Vec3 = ray.direction().normalized();
+        let t: f64 = 0.5 * (unit_direction.y() + 1.0);
+        ((1.0 - t) * Color::new(1.0, 1.0, 1.0)) + (t * Color::new(0.5, 0.7, 1.0))
+    }
+}
 
 fn main() {
-    // Consts for image dimensions.
-    const IMAGE_WIDTH: u64 = 256;
-    const IMAGE_HEIGHT: u64 = 256;
+    let aspect_ratio: f64 = 16.0 / 9.0;
+    let image_width: u64 = 1920;
+    let image_height: u64 = ((image_width as f64) / aspect_ratio) as u64;
+    let samples_per_pixel: u64 = 100;
+    let max_depth: u64 = 25;
+
+    // World
+
+    let mut world: HittableList = HittableList::new(vec![]);
+
+    world.add(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
+
+    world.add(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
+
+    // Camera
+
+    let camera: Camera = Camera::new();
 
     // PPM Headers.
+
     println!("P3");
-    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("{} {}", image_width, image_height);
     println!("255");
 
-    for h in (0..IMAGE_HEIGHT).rev() {
-        eprint!("\rScanlines remaining: {:3}", IMAGE_HEIGHT - h - 1);
-        stderr().flush().unwrap();
+    let mut rng = rand::thread_rng();
 
-        for w in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new(
-                (w as f64) / ((IMAGE_WIDTH - 1)  as f64),
-                (h as f64) / ((IMAGE_HEIGHT - 1) as f64),
-                0.25
-            );
+    for h in (0..image_height as u32).rev().progress() {
+        for w in 0..image_width {
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..samples_per_pixel {
+                let random_u: f64 = rng.gen();
+                let random_v: f64 = rng.gen();
 
-            println!("{}", pixel_color.format_color());
+                let u = (w as f64 + random_u) / ((image_width - 1) as f64);
+                let v = (h as f64 + random_v) / ((image_height - 1) as f64);
+
+                let r: Ray = camera.get_ray(u, v);
+                pixel_color += ray_color(&r, &world, max_depth);
+            }
+            
+            println!("{}", pixel_color.format_color(samples_per_pixel));
         }
     }
 }
